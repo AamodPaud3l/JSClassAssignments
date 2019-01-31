897ed669294b134670bbfebeaafec38ebaff174a
Aamod Paudel <paudelaamod@gmail.com>
2019-01-31 19:53:04 +0545

    Assignment 1

---
+++ Assignment 1/qn2.js
@@ -0,0 +1,14 @@
+/* Q2. Calculate properties of a circle, using the definitions here.
+
+Store a radius into a variable.
+Calculate the circumference based on the radius, and output "The circumference is NN".
+Calculate the area based on the radius, and output "The area is NN". */
+
+"use strict";
+
+var radius = 5;
+var circum = 2 * (22/7) * radius;
+var area = (22/7) * (radius*radius);
+
+console.log("The circumference is ", circum);
+console.log("The area is ", area);
